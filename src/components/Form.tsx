0170c2267ba9b8531c75939fc2126d310d4c973a
wiresnchains <dgrachovsd@gmail.com>
2026-02-07 11:39:06 +0100

    Update form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,21 @@
-import { forwardRef, HTMLInputTypeAttribute, MouseEventHandler, ReactNode, useRef, useState } from 'react';
 import clsx from 'clsx';
+import { FormEvent, forwardRef, HTMLInputTypeAttribute, MouseEventHandler, ReactNode, useRef, useState } from 'react';
 import { Alert } from './Alert';
 import { useTranslation } from '../hooks/use-locale';
+import { BaseParentComponent } from '../types';
 
 import '../styles/components/Form.scss';
 
-interface FormProps {
-    children?: ReactNode;
+interface FormProps extends BaseParentComponent {
+    onSubmit?(e: FormEvent<HTMLFormElement>): void;
 }
 
 export function Form(props: FormProps) {
-    return <div className="form">{props.children}</div>;
+    return (
+        <form className="form" onSubmit={props.onSubmit}>
+            {props.children}
+        </form>
+    );
 }
 
 interface FormRowProps {
@@ -68,8 +73,75 @@ export function ContactForm() {
 
     const translation = useTranslation();
 
+    const submit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const nameInput = nameInputRef.current;
+        const emailInput = emailInputRef.current;
+        const messageInput = messageInputRef.current;
+
+        if (!nameInput || !emailInput || !messageInput) {
+            setError(translation.contantForm.error);
+            return;
+        }
+
+        if (!nameInput.value || !emailInput.value || !messageInput.value) {
+            setError(translation.contantForm.emptyFields);
+            return;
+        }
+
+        try {
+            let payload = {
+                embeds: [
+                    {
+                        title: 'Contact Form',
+                        timestamp: new Date().toISOString(),
+                        fields: [
+                            {
+                                name: 'Name',
+                                value: nameInput.value,
+                                inline: true,
+                            },
+                            {
+                                name: 'E-mail',
+                                value: emailInput.value,
+                            },
+                            {
+                                name: 'Message',
+                                value: messageInput.value,
+                            },
+                        ],
+                        footer: {
+                            text: "User's local time",
+                        },
+                    },
+                ],
+            };
+
+            let result = await fetch(import.meta.env.VITE_CONTACT_DISCORD_WEBHOOK_URL as string, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            });
+
+            if (!result.ok) setError(translation.contantForm.failed);
+
+            nameInput.value = '';
+            emailInput.value = '';
+            messageInput.value = '';
+
+            setError(undefined);
+            setSuccess(translation.contantForm.sent);
+        } catch (err) {
+            console.error(err);
+            setError(translation.contantForm.failed);
+        }
+    };
+
     return (
-        <Form>
+        <Form onSubmit={submit}>
             <FormRow>
                 <Input type="text" placeholder={translation.contantForm.name} ref={nameInputRef} />
                 <Input type="email" placeholder={translation.contantForm.email} ref={emailInputRef} />
@@ -78,74 +150,7 @@ export function ContactForm() {
                 <Textarea placeholder={translation.contantForm.message} ref={messageInputRef} />
             </FormRow>
             <FormRow>
-                <Button
-                    type="primary"
-                    submit
-                    onClick={async () => {
-                        const nameInput = nameInputRef.current;
-                        const emailInput = emailInputRef.current;
-                        const messageInput = messageInputRef.current;
-
-                        if (!nameInput || !emailInput || !messageInput) {
-                            setError(translation.contantForm.error);
-                            return;
-                        }
-
-                        if (!nameInput.value || !emailInput.value || !messageInput.value) {
-                            setError(translation.contantForm.emptyFields);
-                            return;
-                        }
-
-                        try {
-                            let payload = {
-                                embeds: [
-                                    {
-                                        title: 'Contact Form',
-                                        timestamp: new Date().toISOString(),
-                                        fields: [
-                                            {
-                                                name: 'Name',
-                                                value: nameInput.value,
-                                                inline: true,
-                                            },
-                                            {
-                                                name: 'E-mail',
-                                                value: emailInput.value,
-                                            },
-                                            {
-                                                name: 'Message',
-                                                value: messageInput.value,
-                                            },
-                                        ],
-                                        footer: {
-                                            text: "User's local time",
-                                        },
-                                    },
-                                ],
-                            };
-
-                            let result = await fetch(import.meta.env.VITE_CONTACT_DISCORD_WEBHOOK_URL as string, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                                body: JSON.stringify(payload),
-                            });
-
-                            if (!result.ok) setError(translation.contantForm.failed);
-
-                            nameInput.value = '';
-                            emailInput.value = '';
-                            messageInput.value = '';
-
-                            setError(undefined);
-                            setSuccess(translation.contantForm.sent);
-                        } catch (err) {
-                            console.error(err);
-                            setError(translation.contantForm.failed);
-                        }
-                    }}
-                >
+                <Button type="primary" submit>
                     {translation.contantForm.send}
                 </Button>
             </FormRow>
