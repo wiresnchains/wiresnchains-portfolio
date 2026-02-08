e74333ec2c41fc212d250a546fa7364c6c3ffff4
wiresnchains <dgrachov@icloud.com>
2026-02-08 21:05:39 +0100

    Add table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+import { BaseParentComponent } from '../types';
+import '../styles/components/Table.scss';
+
+export function Table(props: BaseParentComponent) {
+    return <table className={clsx('table', props.utility)}>{props.children}</table>;
+}
+
+export function TableRow(props: BaseParentComponent) {
+    return <tr className={clsx('table-row', props.utility)}>{props.children}</tr>;
+}
+
+export function TableHeader(props: BaseParentComponent) {
+    return <th className={clsx('table-header', props.utility)}>{props.children}</th>;
+}
+
+export function TableData(props: BaseParentComponent) {
+    return <td className={clsx('table-data', props.utility)}>{props.children}</td>;
+}
