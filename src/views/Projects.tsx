4664ad3c609ffe23dabfb7361696e7b6a309c369
wiresnchains <dgrachovsd@gmail.com>
2026-02-08 19:45:48 +0100

    Add project images

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,6 +1,19 @@
-import { Card } from '../components/Card';
+import { Card, CardGallery } from '../components/Card';
 import { Inline } from '../components/Inline';
 import { useTranslation } from '../hooks/use-locale';
+
+import planfond01 from '../assets/images/planfond/planfond-01.webp';
+import planfond02 from '../assets/images/planfond/planfond-02.webp';
+import planfond03 from '../assets/images/planfond/planfond-03.webp';
+import planfond04 from '../assets/images/planfond/planfond-04.webp';
+import planfond05 from '../assets/images/planfond/planfond-05.webp';
+import planfond06 from '../assets/images/planfond/planfond-06.webp';
+import slidehub01 from '../assets/images/slidehub/slidehub-01.webp';
+import slidehub02 from '../assets/images/slidehub/slidehub-02.webp';
+import slidehub03 from '../assets/images/slidehub/slidehub-03.webp';
+import slidehub04 from '../assets/images/slidehub/slidehub-04.webp';
+import slidehub05 from '../assets/images/slidehub/slidehub-05.webp';
+import slidehub06 from '../assets/images/slidehub/slidehub-06.webp';
 
 export function Projects() {
     const translation = useTranslation();
@@ -27,6 +40,16 @@ export function Projects() {
                 </Card>
 
                 <Card>
+                    <CardGallery
+                        images={[
+                            { src: planfond01, alt: 'planfond-01' },
+                            { src: planfond02, alt: 'planfond-02' },
+                            { src: planfond03, alt: 'planfond-03' },
+                            { src: planfond04, alt: 'planfond-04' },
+                            { src: planfond05, alt: 'planfond-05' },
+                            { src: planfond06, alt: 'planfond-06' },
+                        ]}
+                    />
                     <h3>Planfond</h3>
                     <small className="muted">2025 - {translation.common.now}</small>
                     <p>{translation.projectDescriptions.planfond}</p>
@@ -36,6 +59,16 @@ export function Projects() {
                 </Card>
 
                 <Card>
+                    <CardGallery
+                        images={[
+                            { src: slidehub01, alt: 'slidehub-01' },
+                            { src: slidehub02, alt: 'slidehub-02' },
+                            { src: slidehub03, alt: 'slidehub-03' },
+                            { src: slidehub04, alt: 'slidehub-04' },
+                            { src: slidehub05, alt: 'slidehub-05' },
+                            { src: slidehub06, alt: 'slidehub-06' },
+                        ]}
+                    />
                     <h3>Slidehub</h3>
                     <small className="muted">2025</small>
                     <p>{translation.projectDescriptions.slidehub}</p>
